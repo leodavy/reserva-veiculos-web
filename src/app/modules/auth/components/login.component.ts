5b54e5d77fccfc8cc714e130972ed9b0a08563de
Davy Leonardo <leo.davy@hotmail.com>
2024-05-27 07:36:01 -0300

    correção do nome

--- src/app/modules/auth/components/login.component.ts
+++ src/app/modules/auth/components/login.component.ts
@@ -22,8 +22,8 @@ import { MenuItem } from '../../../shared/model/menu-item';
     <h2 class="text-2xl font-bold mb-6 text-center">Login</h2>
     <form>
       <div class="mb-4">
-        <label for="email" class="block text-sm font-medium text-gray-700">Email</label>
-        <input type="email" id="email" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
+        <label for="login" class="block text-sm font-medium text-gray-700">Usuário</label>
+        <input type="login" id="login" class="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" required>
       </div>
       <div class="mb-6">
         <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
