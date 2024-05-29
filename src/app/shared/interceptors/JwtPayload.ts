10130e9debd9982321e2607234a95084cee56703
Davy Leonardo <leo.davy@hotmail.com>
2024-05-29 01:14:06 -0300

    correção dos atributos do JwtPayload

--- src/app/shared/interceptors/JwtPayload.ts
+++ src/app/shared/interceptors/JwtPayload.ts
@@ -1,11 +1,7 @@
 export interface JwtPayload {
-    sub: string;
-    iss: string;
-    exp: number;
     payload: {
-      usuNrId: number;
-      usuTxNome: string;
-      roles: string[];
+        usuNrId: number;
+        usuTxNome: string;
+        roles: string[];
     };
-  }
-  
+}
